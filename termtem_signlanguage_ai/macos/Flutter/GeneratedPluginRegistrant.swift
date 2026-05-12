3cc9995f779bbb98cb8038dab81d41e2f35ae63f
Sorasit Kateratorn <sorasit.ka@ku.th>
2026-05-12 15:41:29 +0700

    feat: add video preview gadget and confidence overlay

--- termtem_signlanguage_ai/macos/Flutter/GeneratedPluginRegistrant.swift
+++ termtem_signlanguage_ai/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import file_picker
 import file_selector_macos
 import speech_to_text
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
+  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
 }
